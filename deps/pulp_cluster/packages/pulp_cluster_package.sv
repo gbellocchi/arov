12de3cb11cfb92d0ad16ce25ca1a04d0152ac4e1
Gianluca Bellocchi <gbellocchi@badile14.ee.ethz.ch>
2022-07-06 17:51:37 +0200

    pulp_cluster - Re-established original peripherals mapping. First HWPE is mapped to ID=4. Subsequent HWPEs are mapped from ID=8. See addr_to_pe_idx.sv for additional information.

--- deps/pulp_cluster/packages/pulp_cluster_package.sv
+++ deps/pulp_cluster/packages/pulp_cluster_package.sv
@@ -19,13 +19,13 @@ package pulp_cluster_package;
   parameter NB_SPERIPH_PLUGS_EU  = 2;
   
   // position of peripherals on slave port of periph interconnect
-  parameter SPER_EOC_ID      = 0; // 0x400
-  parameter SPER_TIMER_ID    = 1; // 0x400
-  parameter SPER_EVENT_U_ID  = 2; // 0x400? 0x800?
-  parameter SPER_EU_HWPE_ID  = 4; // 0x400 -> Disactivated at the moment!
-  parameter SPER_ICACHE_CTRL = 5; // 0x400
-  parameter SPER_DMA_ID      = 6; // 0x400
-  parameter SPER_EXT_ID      = 7; // 0x400
+  parameter SPER_EOC_ID      = 0;
+  parameter SPER_TIMER_ID    = 1;
+  parameter SPER_EVENT_U_ID  = 2;
+  parameter SPER_HWPE_ID     = 4;
+  parameter SPER_ICACHE_CTRL = 5;
+  parameter SPER_DMA_ID      = 6;
+  parameter SPER_EXT_ID      = 7;
   
   // if set to 1, then instantiate APU in the cluster
   parameter APU_CLUSTER = 0;
