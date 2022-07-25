a340811f169da8ae93ec270fa0cf01ab993bf85b
Gianluca Bellocchi <gbellocchi@badile14.ee.ethz.ch>
2022-07-25 15:07:33 +0200

    Added FPGA build utils for ZCU104 and Ultra96V2.

---
+++ fpga/utils/hero/zcu104/hero_exilzcu104_export_hw.tcl
@@ -0,0 +1,36 @@
+# =====================================================================
+# Project:      HERO
+# Title:        hero_exilzcu104_export_hw.tcl
+# Description:  TCL script to export HW design files.
+#
+# $Date:        15.09.2021
+# =====================================================================
+#
+# Copyright (C) 2021 University of Modena and Reggio Emilia..
+#
+# Author: Gianluca Bellocchi, University of Modena and Reggio Emilia.
+#
+# =====================================================================
+# Design name.
+set design_name [lindex $argv 0]
+puts "Design name - $design_name"
+
+# Build directory
+set build_dir [lindex $argv 1]
+
+# Project directory
+set prj_dir "$build_dir/$design_name/vivado_prj"
+puts "Vivado project directory - $prj_dir"
+
+# Open project
+open_project ${prj_dir}/hero_exilzcu104.xpr
+
+# Export Hardware Definition file.
+file mkdir ${prj_dir}/hero_exilzcu104.sdk
+write_hwdef -force  -file ${prj_dir}/hero_exilzcu104.sdk/hero_exilzcu104_wrapper.hdf
+
+# Export bitstream file.
+file copy -force ${prj_dir}/hero_exilzcu104.runs/impl_1/hero_exilzcu104_wrapper.bit ${prj_dir}/hero_exilzcu104.sdk/hero_exilzcu104_wrapper.bit
+
+# Export Xilinx Support Archive (XSA) file.
+write_hw_platform -fixed -force  -include_bit -file ${prj_dir}/hero_exilzcu104_wrapper.xsa
